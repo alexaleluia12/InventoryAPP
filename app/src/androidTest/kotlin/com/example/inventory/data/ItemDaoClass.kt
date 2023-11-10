6a7454bc1ce5bbbbdbd476f863f7fdbab1c444bd
alexaleluia12 <alexdiasaleluia@gmail.com>
2023-11-10 15:53:37 -0300

    adiciona teste instrumental do dao

---
+++ app/src/androidTest/kotlin/com/example/inventory/data/ItemDaoClass.kt
@@ -0,0 +1,69 @@
+package com.example.inventory.data
+
+import android.content.Context
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
+import org.junit.After
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.io.IOException
+import kotlin.jvm.Throws
+
+@RunWith(AndroidJUnit4::class)
+class ItemDaoClass {
+    private lateinit var itemDao: ItemDao
+    private lateinit var inventoryDatabase: InventoryDatabase
+
+    private var item1 = Item(1, "banana", 10.0, 20)
+    private var item2 = Item(2, "apple", 15.0, 25)
+
+    @Before
+    fun createDB() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        // banco em memoria apenas para teste, eh deletado depois de tudo
+        inventoryDatabase = Room.inMemoryDatabaseBuilder(context, InventoryDatabase::class.java)
+            .allowMainThreadQueries()
+            .build()
+
+        itemDao = inventoryDatabase.itemDao()
+    }
+
+    @After
+    @Throws(IOException::class)
+    fun closeDB() {
+        inventoryDatabase.close()
+    }
+
+    private suspend fun addOneItemToDB() {
+        itemDao.insert(item1)
+    }
+
+    private suspend fun addTowItemToDB() {
+        itemDao.insert(item1)
+        itemDao.insert(item2)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun daoInsert_insertItensIntoDB() = runBlocking {
+        addOneItemToDB()
+        val allItems = itemDao.getAllItems().first()
+        Assert.assertEquals(allItems[0], item1)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun daoGetAllItems_returnAllItemsFromDB() = runBlocking {
+        addTowItemToDB()
+        val allItems = itemDao.getAllItems().first()
+        Assert.assertEquals(
+            allItems[0], item2
+        )
+        Assert.assertEquals(allItems[1], item1)
+    }
+}
